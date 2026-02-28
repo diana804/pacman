e5926894ec42490fcd64ac80fa863c449ae1f509
diana804 <57040062+diana804@users.noreply.github.com>
2026-02-27 21:13:12 -0500

    Create constants.dart

---
+++ constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const int kMapWidth = 15;
+const int kMapHeight = 9;
+
+const Color kWallColor = Color(0xFF1565C0);
+const Color kWallBorderColor = Color(0xFF42A5F5);
+const Color kDotColor = Colors.yellow;
+const Color kPowerDotColor = Colors.orange;
+const Color kPacmanColor = Colors.yellow;
+const Color kEyeColor = Colors.black;
+
+const double kGameSpeedMs = 200.0;
+const int kNormalDotPoints = 10;
+const int kPowerDotPoints = 50;
